3c479796a80f2a2627ce8665e08033b6a855ee8e
LanderU <lander.usategui@gmail.com>
2017-01-14 23:01:45 +0100

    temperature: Added simple example

---
+++ temperature/readTemperature.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import spidev
+from MPU9250 import MPU9250
+from subprocess import call
+import commands
+import time
+
+res = commands.getstatusoutput('whoami')
+
+if res [1] == 'root':
+	# Initialize the driver
+	imu = MPU9250()
+	imu.initialize()
+	# Read Temperature
+	while True:
+		print "{00:.2f}".format(imu.read_temp())
+		time.sleep(3)
+else:
+	print 'Execute this script with \"sudo\"'
